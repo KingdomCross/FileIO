f236751e4255cd8014b0ce5fd32bc37ed1014806
KingdomCross <alexchheng1998@yahoo.com>
2019-10-16 22:51:15 -0700

    Arguments added sales.txt added block comment with goal to convert usable

--- src/HotelTest.java
+++ src/HotelTest.java
@@ -13,5 +13,40 @@ public class HotelTest {
         Scanner scanner = new Scanner(System.in);
         String filename = scanner.nextLine();
         File file = new File("");
+
+        //Convert to usable code goal
+        /*if(args.length < 2){
+            System.out.println("Arguments not exist");
+            System.exit(0);
+        }
+
+        String inFile = args[0];
+        String outFile = args[1];
+
+        File file = new File(inFile); //"table.txt"
+        if (!file.exists()) {
+            System.out.println("File not exist");
+        }
+        else{
+            PrintWriter out = null;
+            try {
+                out = new PrintWriter(outFile); //"output.txt"
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            Scanner in = null;
+            try {
+                in = new Scanner(file);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            while (in.hasNext()) {
+                out.println(in.nextLine());
+            }
+            in.close();
+            out.close();
+        }*/
     }
 }
