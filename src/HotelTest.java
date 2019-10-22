94c2bae864b51999b205f267644afe64d5c9d488
Chloe Culver <c.culver831@gmail.com>
2019-10-22 15:22:44 -0700

    I did it

--- src/HotelTest.java
+++ src/HotelTest.java
@@ -19,98 +19,106 @@ public class HotelTest {
     	 */
     	
 		if(args.length == 0) {
+			System.out.println("No Arguments passed");
 			Scanner inpt = new Scanner(System.in);
 			System.out.println("Enter number of sales: ");
 			int choice = inpt.nextInt();
 			String input = "";
 			System.out.println("Enter the sales: ");
+			/**
+			 * Takes contents of arguments and adds them into one string
+			 */
 			for(int i = 0; i < choice ; i++) 
 			{
 				input += inpt.next() + "\n";
 			}	
 			try {
 				System.out.println("Writing contents onto new File... ");
-				/**
-				 * Takes contents of arguments and adds them into one string
-				 */
-				PrintWriter sales = new PrintWriter("sales.txt");
-
-				/**
-				 * puts string contents in a scanner class object to be written into the sales.txt file
-				 */
-				Scanner in = new Scanner(input);
-				Scanner e = new Scanner(input);
-				/**
-				 * writes scanner info onto new file called sales.txt
-				 */
-				while(in.hasNext()) 
-				{
-					sales.println(in.nextLine());
+				
+				File data = new File("C:\\Users\\Chloe Culver\\eclipse-workspace\\Stream IO\\Whales.txt");
+				if(!data.exists()){
+					System.out.println("File does not exist");
 				}
-				
-				
-				/**
-				 * 	Code for Part II
-				 */
-				
-				/**
-				 * Instantiate objects for part 2
-				 */
-				String str1 = "Conference";
-				String str2 = "Dinner";
-				String str3 = "Lodging";
-				/**
-				 * Creates writable files Conference and Data
-				 */
-				PrintWriter Conference = new PrintWriter("Conference.txt");
-				PrintWriter Dinner = new PrintWriter("Dinner.txt");
-				PrintWriter Lodging = new PrintWriter("Lodging.txt");
-				String LastLine = null;
-				while(e.hasNext()) {
-					String line = e.nextLine();
+				else{
 					/**
-					 * Checks each line if it contains the word Conference,
-					 * if it does. then that line and the line before it it written down in the Conference.txt file
+					 * puts string contents in a scanner class object to be written into the sales.txt file
 					 */
-					if(line.contains(str1)) 
+					PrintWriter sales = new PrintWriter(data);
+					Scanner in = new Scanner(input);
+					Scanner e = new Scanner(input);
+					/**
+					 * writes scanner info onto new file called sales.txt
+					 */
+					while(in.hasNext()) 
 					{
+						sales.println(in.nextLine());
+					}
 					
-						Conference.println(line);
+					
+					/**
+					 * 	Code for Part II
+					 */
+					
+					/**
+					 * Instantiate objects for part 2
+					 */
+					String str1 = "Conference";
+					String str2 = "Dinner";
+					String str3 = "Lodging";
+					/**
+					 * Creates writable files Conference and Data
+					 */
+					PrintWriter Conference = new PrintWriter("Conference.txt");
+					PrintWriter Dinner = new PrintWriter("Dinner.txt");
+					PrintWriter Lodging = new PrintWriter("Lodging.txt");
+					String LastLine = null;
+					while(e.hasNext()) {
+						String line = e.nextLine();
+						/**
+						 * Checks each line if it contains the word Conference,
+						 * if it does. then that line and the line before it it written down in the Conference.txt file
+						 */
+						if(line.contains(str1)) 
+						{
+						
+							Conference.println(line);
+						}
+						/**
+						 * Checks each line if it contains the word Dinner,
+						 * if it does. then that line and the line before it it written down in the Dinner.txt file
+						 */
+						if(line.contains(str2)) 
+						{
+						
+							Dinner.println(line);
+						}
+						/**
+						 * Checks each line if it contains the word Lodging,
+						 * if it does. then that line and the line before it it written down in the Lodging.txt file
+						 */
+						if(line.contains(str3))
+						{
+						
+							Lodging.println(line);
+						}
+						/**
+						 * gets the previous line iterated
+						 */
+						LastLine = line;
 					}
 					/**
-					 * Checks each line if it contains the word Dinner,
-					 * if it does. then that line and the line before it it written down in the Dinner.txt file
+					 * Closes all the previous files open
 					 */
-					if(line.contains(str2)) 
-					{
-					
-						Dinner.println(line);
+					Lodging.close();
+					Conference.close();
+					Dinner.close();
+					e.close();
+					in.close();
+					sales.close();
+					System.out.println("Files Orgnaized Successfully.");
 					}
-					/**
-					 * Checks each line if it contains the word Lodging,
-					 * if it does. then that line and the line before it it written down in the Lodging.txt file
-					 */
-					if(line.contains(str3))
-					{
-					
-						Lodging.println(line);
-					}
-					/**
-					 * gets the previous line iterated
-					 */
-					LastLine = line;
 				}
-				/**
-				 * Closes all the previous files open
-				 */
-				Lodging.close();
-				Conference.close();
-				Dinner.close();
-				e.close();
-				in.close();
-				sales.close();
-				System.out.println("Files Orgnaized Successfully.");
-				}
+				
 			/**
 			 * Exception handler if no file can be written to
 			 */
